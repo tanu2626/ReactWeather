4e929ea6c4dd253af3e2be503ef433e657aa5600
Tanu <tanuuu.21@gmail.com>
2017-01-03 02:10:55 +1100

    Added copy

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -3,7 +3,10 @@ var React = require('react');
 var Examples = React.createClass({
    render: function() {
      return (
+       <div>
        <h3> Examples Component!</h3>
+       <p> Welcome to examples page </p>
+       </div>
      )
    }
 
